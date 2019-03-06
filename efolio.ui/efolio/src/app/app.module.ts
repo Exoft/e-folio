2e9e28922308e91b4e03c32be0a7420790e723b4
Shevchenko Andriy <shewchenkoandriy@gmail.com>
2019-03-06 12:14:39 +0200

    add login component

--- efolio.ui/efolio/src/app/app.module.ts
+++ efolio.ui/efolio/src/app/app.module.ts
@@ -4,12 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: 'login', component: LoginComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+    ),
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
@@ -18,3 +29,4 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+ 
