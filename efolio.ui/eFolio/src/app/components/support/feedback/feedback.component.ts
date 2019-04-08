dc62991a2fd6915b43920391737e0e8de7aa7b67
brednyeva <torautowaidu@gmail.com>
2019-04-08 12:02:01 +0300

    email controller + email service

--- efolio.ui/eFolio/src/app/components/support/feedback/feedback.component.ts
+++ efolio.ui/eFolio/src/app/components/support/feedback/feedback.component.ts
@@ -1,20 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {LoaderService} from '../../../services/loader.service';
+import {MatSnackBar} from '@angular/material';
+import {UserLoggingService} from '../../../services/user-logging.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-feedback',
   templateUrl: './feedback.component.html',
   styleUrls: ['./feedback.component.scss']
 })
-export class FeedbackComponent {
+export class FeedbackComponent implements OnInit {
+public feedbackform  = new FormGroup({
+  name: new FormControl(null),
+  email: new FormControl(null, [Validators.email, Validators.required]),
+  message: new FormControl(null, [Validators.required])
+});
 
-  public feedbackForm = new FormGroup({
-    name: new FormControl(null),
-    email: new FormControl(null, [Validators.email, Validators.required]),
-    message: new FormControl(null, [Validators.required])
-  });
-
-
+ // constructor() { }
   constructor() { }
+
+  ngOnInit() {}
 
 }
--- efolio.ui/eFolio/src/app/components/support/support.component.ts
+++ efolio.ui/eFolio/src/app/components/support/support.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LoaderService} from '../../services/loader.service';
 
 @Component({
   templateUrl: './support.component.html',
   styleUrls: ['./support.component.scss']
 })
-export class SupportComponent {
+export class SupportComponent implements OnInit {
+  constructor(private loaderService: LoaderService) { }
 
-  constructor() { }
+  ngOnInit() {
+    this.loaderService.stopLoading();
+  }
+
 }
