3eab6dadbae549b315894fb8da0ac927d153b65e
brednyeva <torautowaidu@gmail.com>
2019-04-08 12:02:01 +0300

    email controller + email service

---
+++ BL/Services/EmailService.cs
@@ -0,0 +1,36 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using MimeKit;
+//using MailKit.Net.Smtp;
+//using System.Threading.Tasks;
+
+
+//namespace eFolio.BL.Services
+//{
+//   public class EmailService
+//    {
+
+//        public async Task SendEmailAsync(string email, string name, string message)
+//        {
+//            var emailMessage = new MimeMessage();
+
+//            emailMessage.From.Add(new MailboxAddress(name, email));
+//            emailMessage.To.Add(new MailboxAddress("Efolio", "torautowaidu@gmail.com"));
+//            emailMessage.Subject = "Customer support";
+//            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+//            {
+//                Text = message
+//            };
+
+//            using (var client = new SmtpClient())
+//            {
+//                await client.ConnectAsync("smtp.gmail.com", 25, false);
+//                //await client.AuthenticateAsync("torautowaidu@gmail.com", "password");
+//                await client.SendAsync(emailMessage);
+
+//                await client.DisconnectAsync(true);
+//            }
+//        }
+//    }
+//}
--- efolio.ui/eFolio/src/app/components/support/contact-info/contact-info.component.ts
+++ efolio.ui/eFolio/src/app/components/support/contact-info/contact-info.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LoaderService} from '../../../services/loader.service';
 
 @Component({
   selector: 'app-contact-info',
@@ -6,7 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-info.component.scss']
 })
 
-export class ContactInfoComponent {
+export class ContactInfoComponent implements OnInit {
 
-  constructor() { }
+  // constructor() { }
+  constructor(private loaderService: LoaderService) { }
+
+  ngOnInit() {
+    this.loaderService.stopLoading();
+  }
 }
