ecac84f96e80f58154eafe8f42d86b9960eeecf5
yuriilevkopz <yuriilevko99@gmail.com>
2019-04-08 15:04:26 +0300

    Added project filter

--- efolio.ui/efolio/src/app/components/projects/project-filter/project-filter.component.ts
+++ efolio.ui/efolio/src/app/components/projects/project-filter/project-filter.component.ts
@@ -1,5 +1,4 @@
 import {Component, Input, Output} from '@angular/core';
-import {ProjectService} from '../../../services/project.service';
 import {EventEmitter} from '@angular/core';
 
 @Component({
@@ -9,11 +8,44 @@ import {EventEmitter} from '@angular/core';
 })
 export class ProjectFilterComponent {
   public searchString;
+  public check1 = false;
+  public check2 = false;
+  public check3 = false;
+  public check4 = false;
   @Output() public SearchStringChanged = new EventEmitter<string>();
-
   constructor() {}
   public onSearchClick() {
     console.log(this.searchString);
     this.SearchStringChanged.emit(this.searchString);
   }
+
+  check1change() {
+    this.check1 = !this.check1;
+    if (this.check1 === true) {
+      this.searchString += ' .Net';
+      } else {this.searchString = this.searchString.replace(' .Net', '');
+    }
+    }
+  check2change() {
+    this.check2 = !this.check2;
+    if (this.check2 === true) {
+      this.searchString += ' Java';
+    } else {this.searchString = this.searchString.replace(' Java', '');
+    }
+  }
+  check3change() {
+    this.check3 = !this.check3;
+    if (this.check3 === true) {
+      this.searchString += ' Angular.';
+    } else {this.searchString = this.searchString.replace(' Angular', '');
+    }
+  }
+  check4change() {
+    this.check4 = !this.check4;
+    if (this.check4 === true) {
+      this.searchString += ' Node';
+    } else {this.searchString = this.searchString.replace(' Node', '');
+    }
+  }
 }
+
